d4cd7a7791e583e34b067df3ce465f52c688c96e
LuccaDi <lucca_di@hotmail.com>
2021-04-26 02:35:31 -0300

    Feature open in new window

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpandChartComponent } from './components/template/expand-chart/expand-chart.component';
 import { HomeComponent } from './components/template/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'expandChart/:id',
+    component: ExpandChartComponent,
   },
 ];
 
