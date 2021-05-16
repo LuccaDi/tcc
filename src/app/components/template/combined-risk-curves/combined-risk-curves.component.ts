f6acc4532d7dd7c25af9b6b10957e15b319da83d
LuccaDi <lucca_di@hotmail.com>
2021-05-16 19:58:40 -0300

    created combined risk curves component

---
+++ src/app/components/template/combined-risk-curves/combined-risk-curves.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-combined-risk-curves',
+  templateUrl: './combined-risk-curves.component.html',
+  styleUrls: ['./combined-risk-curves.component.css']
+})
+export class CombinedRiskCurvesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
